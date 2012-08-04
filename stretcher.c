d64065655267e6130f5f4cac93a577ab9124bbe5
boyska <boyska@gmail.com>
2012-08-04 03:35:14 +0200

    stretcher (was: hash) has clean behaviour
    No frills, usage, easily usable in scripts

--- stretcher.c
+++ stretcher.c
@@ -13,10 +13,19 @@
 #include "warn.h"
 #include "base64.h"
 
-//TODO: separate into a clean calculate_pass(passwd, passwdlen, N, r, p, salt, saltlen) <-- this is crypto_scrypt !!
-//TODO: design a useful interface!
+/* ***********************************
+ * scrypt-strecth
+ *
+ * with no parameters (or, TODO, with just maxresource parameters), will
+ *      calculate appropriate parameters and output them
+ * with parameters will stretch the key (which will be expected as input)
+
+ * TODO: params maxmem, maxmemfrac, maxtime
+ * TODO: --batch for the current, machine readable output: otherwise it will be clearer
+*/
 void usage(char *argv[]) {
-	fprintf(stderr, "Usage: %s [logN r p] [saltfile]", argv[0]);
+	fprintf(stderr, "Usage:\t%s\n"
+			        "      \t%s logN r p saltfile\n", argv[0], argv[0]);
 }
 
 int main(int argc, char *argv[]) {
@@ -25,39 +34,45 @@ int main(int argc, char *argv[]) {
 	uint32_t r, p;
 	size_t maxmem = 0;
 	double maxmemfrac = 0.5;
-	double maxtime = 10.0;
+	double maxtime = 5.0;
 	int rc;
 	uint8_t *salt;
 	uint8_t dk[64]; //derived key
-	uint8_t passwd[4];
+	uint8_t *passwd;
 	char *salt_encoded = NULL;
 	FILE *f;
 	char *dk_be64;
 
 	int count;
-	for(count=0; count < 4; count++)
-		passwd[count]=(uint8_t)count;
 
 	/* Pick values for N, r, p. */
 	if(argc == 1) {
 		if ((rc = pickparams(maxmem, maxmemfrac, maxtime,
 			&logN, &r, &p)) != 0)
 			return (rc);
-	} else if (argc >= 4) {
-		sscanf(argv[1], "%d", &logN);
-		sscanf(argv[2], "%" SCNu32, &r);
-		sscanf(argv[3], "%" SCNu32, &p);
-	} else {
+		salt = calloc(32, sizeof(uint8_t));
+		if ((rc = getsalt(salt)) != 0)
+			return (rc);
+		be64_encode(salt, 32, &salt_encoded, 0);
+		printf("%d\n%" PRIu32 "\n%" PRIu32 "\n", logN, r, p);
+		printf("%s\n", salt_encoded);
+		free(salt_encoded);
+		return 0;
+	} 
+	if( argc < 5) {
 		usage(argv);
 		return 127;
 	}
+	sscanf(argv[1], "%d", &logN);
+	sscanf(argv[2], "%" SCNu32, &r);
+	sscanf(argv[3], "%" SCNu32, &p);
+	sscanf(argv[4], "%ms", &salt_encoded);
+	fprintf(stderr, "Insert password: ");
+	scanf("%ms", &passwd);
 	N = (uint64_t)(1) << logN;
 
-	fprintf(stderr, "Param:\t%d=>%lu %" PRIu32 " %" PRIu32 "\n", logN, N, r, p);
 	//TODO: stop saltfile madness! we want a base64encoding as a parameter!
 	if(argc >= 5) {
-		printf("Reading salt base64 encoding...\n");
-		sscanf(argv[4], "%ms", &salt_encoded);
 	}
 	if(salt_encoded) { //got salt as input
 		if(be64_decode(salt_encoded, &salt) < 32) {
@@ -67,12 +82,6 @@ int main(int argc, char *argv[]) {
 		free(salt_encoded);
 
 	} else {
-		salt = calloc(32, sizeof(uint8_t));
-		if ((rc = getsalt(salt)) != 0)
-			return (rc);
-		be64_encode(salt, 32, &salt_encoded, 0);
-		printf("Salt is (encoded): %s\n---\n", salt_encoded);
-		free(salt_encoded);
 	}
 
 /*    printf("Salt is: ");*/
@@ -81,7 +90,6 @@ int main(int argc, char *argv[]) {
 	}
 /*    printf("Done!\n");*/
 
-	fprintf(stderr, "Param: %d %lu %d %d\n", logN, N, r, p);
 	/* Generate the derived keys. */
 	for(count=0; count < 64; count++)
 		dk[count] = 0;
@@ -90,8 +98,7 @@ int main(int argc, char *argv[]) {
 		return (3);
 	free(salt);
 	be64_encode(dk, 32, &dk_be64, 0);
-	printf("derived key is...%s", dk_be64);
+	printf("%s", dk_be64);
 	free(dk_be64);
-	printf("\nDone! security is now f***ed up\n");
 	return 0;
 }
